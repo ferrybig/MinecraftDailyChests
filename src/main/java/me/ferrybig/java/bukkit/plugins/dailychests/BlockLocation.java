705d1a797e21992b090f50fbbf2606c9cd2a94aa
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-01-06 23:59:40 +0100

    Forgot to remove licence header from BlockLocation, this update removes the header

--- src/main/java/me/ferrybig/java/bukkit/plugins/dailychests/BlockLocation.java
+++ src/main/java/me/ferrybig/java/bukkit/plugins/dailychests/BlockLocation.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package me.ferrybig.java.bukkit.plugins.dailychests;
 
 import java.io.Serializable;
